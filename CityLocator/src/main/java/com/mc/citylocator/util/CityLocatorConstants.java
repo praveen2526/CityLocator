c0c61a5a3446a2327f72472d3be05fde3bda8667
praveen2526 <69581574+praveen2526@users.noreply.github.com>
2020-08-12 21:47:07 -0700

    Update CityLocatorConstants.java

--- CityLocator/src/main/java/com/mc/citylocator/util/CityLocatorConstants.java
+++ CityLocator/src/main/java/com/mc/citylocator/util/CityLocatorConstants.java
@@ -1,7 +1,7 @@
 package com.mc.citylocator.util;
 
 public class CityLocatorConstants {
-	public static final String CITY_NAME_REGEX = "[a-zA-Z]{1,50}";
+	public static final String CITY_NAME_REGEX = "[a-zA-Z ]{1,50}";
 	public static final String YES = "yes";
 	public static final String NO = "no";
 	public static final String COMMA_SPACE_SEPERATOR = ", ";
